95a9e8d6d36cca26471a8744a88b7551dc97f61d
jensadev <jensandreasson77@gmail.com>
2024-02-22 15:28:02 +0100

    ✨ programmering

--- src/Kemi.tsx
+++ src/Kemi.tsx
@@ -1,6 +1,4 @@
-import { Link } from "react-router-dom";
 import { useNavigate } from 'react-router-dom'
-import { useState } from "react";
 import JSConfetti from 'js-confetti';
 
 function Kemi() {
--- src/Programmering.tsx
+++ src/Programmering.tsx
@@ -1,7 +1,57 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom'
+import JSConfetti from 'js-confetti';
+import codeimg from './assets/code.webp'
+import { useState } from 'react';
 
 function Programmering() {
-  return <div>Programmering- <Link to="/fysik">Fysik</Link></div>;
+  const navigate = useNavigate()
+  const jsConfetti = new JSConfetti()
+  const [number, setNumber] = useState(0);
+
+  const handleClick = () => {
+    if (number === 9) {
+      jsConfetti.addConfetti();
+      setTimeout(() => {
+        navigate('/fysik', { replace: true });
+      }, 3000);
+    }
+  };
+
+  return (
+    <main className="wrapper">
+      <header className="hero region flow">
+        <div className="intro">
+          Snyggt jobbat, nu är det dags för lite
+        </div>
+        <h1>Programmering</h1>
+        <p>Vi gillar att skapa på teknikprogrammet och kod är ett av de verktyg
+          du får lära dig.
+        </p>
+      </header>
+      <section className="region flow">
+        <h2>Upprening, eller iteration</h2>
+        <p>Att kunna upprepa något ett visst antal gånger är en av grunderna i
+          programmering. Det är så användbart i allt (upprepa vadsomhelst X gånger, gå igenom en lista, räkna upp, och ned (inte upp och ned)).
+        </p>
+        <p>Här nedan är ett exempel på en for loop. Det finns även en variabel med namnet final. Vad har final för värde när loopen är färdig?</p>
+        <pre>
+          <code>
+            {`let final
+for (let i = 0; i < 10; i++) {
+  final = i
+}`}
+          </code>
+        </pre>
+        <button onClick={() => setNumber(number - 1)}>-</button>
+        <span>{number}</span>
+        <button onClick={() => setNumber(number + 1)}>+</button>
+        <button onClick={handleClick}>Submit</button>
+
+      </section>
+      {/* <div id="modal" className={countdown !== 5 ? 'modal' : 'hidden'}>
+        <h2 className="red">{countdown}</h2></div> */}
+    </main>
+  )
 }
 
 export default Programmering
